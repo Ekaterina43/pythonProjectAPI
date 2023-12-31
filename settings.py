7796573c42cde3cb1d9822c36429786063d29a2c
Ekaterina43 <126583512+Ekaterina43@users.noreply.github.com>
2023-12-31 17:56:02 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+valid_email = 'sad12@mail.ru'
+valid_password = 'test123'
+
+unvalid_email = 'ghyt@mail.ru'
+unvalid_password = '1234'
+
+unvalid_email1 = ''
+unvalid_password1 = ''
+
+valid_email1 = 'test00@mail.ru'
+valid_password1 = '12345'
+
+
+
